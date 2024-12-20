bdf211422b14f7a3f4525bf370c8fa6eb7956a8c
attentivehumbIe <marvelguardus@gmail.com>
2024-12-21 02:18:18 +0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('clickMe').addEventListener('click', function () {
-    const message = document.getElementById('message');
-    message.textContent = "Хади, ты особенная! 💖 Ты делаешь этот мир лучше!";
-});
