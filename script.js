e6dd5ff3d7be2b8a095600eb7dc02fd2d9bfe840
attentivehumbIe <marvelguardus@gmail.com>
2024-12-21 02:14:10 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('clickMe').addEventListener('click', function () {
+    const message = document.getElementById('message');
+    message.textContent = "Хади, ты особенная! 💖 Ты делаешь этот мир лучше!";
+});
